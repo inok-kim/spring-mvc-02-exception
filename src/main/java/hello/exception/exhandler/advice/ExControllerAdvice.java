8616bc75a99921088a8754d79a9ffc2dc50379ce
iokim <iokim1220@gmail.com>
2022-03-14 20:23:41 +0900

    @ControllerAdvice

---
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,39 @@
+package hello.exception.exhandler.advice;
+
+import hello.exception.api.ApiExceptionController;
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "hello.exception.api")
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex",e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    // ExceptionHandler에 예외를 생략하면 메서드 파라미터의 예외가 지정됨
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex]", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+}
